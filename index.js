4dec7ceb9859910b32e9b2c38e06d1494fd87f4c
Vijaypratapsingh <vijaypratap1747singh@gmail.com>
2025-06-03 19:48:27 +0530

    last2

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-frontend.vercel.app"], 
+ // origin: ["http://localhost:5173", "https://your-frontend.vercel.app"], 
+ origin:"*",
   credentials: true
 }));
 
